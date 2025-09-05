3daf6303476dbe9ab0b77cdfa49207829907a1e1
pollob debnath <pullobdebnath@gmail.com>
2025-09-06 01:06:11 +0600

    first commit

---
+++ login.php
@@ -0,0 +1,19 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Login Form</title>
+</head>
+<body>
+    <form action="#" method="POST">
+        Name : <input type="text" name="name" placeholder="Enter Your Name"> <br><br>
+        Email : <input type="text" email="email" placeholder="Enter Your Email"><br><br>
+        Mobile : <input type="text" mobile="mobile" placeholder="Enter Phone Number"><br><br>
+        City : <input type="text" city="city" placeholder="Enter Your City Name"><br><br>
+        <input type="text" name="submit" placeholder="Submit" value="Submit">
+    </form>
+</body>
+</html>
